1e962d754693679444001b12d9a0fcde438d3089
bhavya07-alt <bhavya.patel072007@gmail.com>
2026-09-11 23:29:46 +0530

    Update PlacementDriveShortlistRank.java

--- src/main/java/OOP/class_problems/PlacementDriveShortlistRank.java
+++ src/main/java/OOP/class_problems/PlacementDriveShortlistRank.java
@@ -61,8 +61,8 @@ class Candidate implements Comparable<Candidate> {
     public static void main(String[] args) {
         Candidate[] candidates = {
                 new Candidate("Aisha", 8.2, 40),
-                new Candidate("Rahul", 6.8, 65),
-                new Candidate("Shreya", 6.0, 90),
+                new Candidate("Rohit", 6.8, 65),
+                new Candidate("Meena", 6.0, 90),
                 new Candidate("Karan", 7.5, 20)
         };
 
