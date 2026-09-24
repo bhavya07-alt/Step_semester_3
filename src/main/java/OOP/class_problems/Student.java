df09b3d5c45999ad808a6e038436b0841d4c0a12
bhavya07-alt <bhavya.patel072007@gmail.com>
2026-09-25 01:35:46 +0530

    session 6 student and college information management problem

---
+++ src/main/java/OOP/class_problems/Student.java
@@ -0,0 +1,29 @@
+package OOP.class_problems;
+
+public class Student {
+
+    String name;
+    double attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ravi", 90);
+        Student student2 = new Student("Anitha", 95);
+
+        printCollegeInfo();
+    }
+}
